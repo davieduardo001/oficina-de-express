b2ca63fc5b3ec8043be268d608dcc84e7597f5ec
DAVI EDUARDO COSTA MIRANDA <davi.emiranda@a.ucb.br>
2023-10-25 09:43:21 -0300

    ...

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sertech-oficina-expressjs
 Repositório de códigos para a oficina de Express.js do evento SerTech da UCB
+
+> se manipula as requisicoes http para a interacao com a web...
+
+
+### node
+* interpretador de JS fora da web (engine fora do browser)
+* pega-se a engine do Chrome (v8) que vai interpretar os codigos JS e TS
---
+++ teste.js
@@ -0,0 +1,26 @@
+function helloNya(string, lenght) {
+    for (let i = 0; i < lenght; i++) {
+        console.log(string)  
+    }    
+}
+
+helloNya("hehehe", 2)
+
+// testes:
+
+let n1 = 3
+const n2 = 5
+
+let n3 = n1 + n2
+
+console.log("a soma eh: " + n3)
+
+// objetos dentro de JS
+let pessoa = {
+    nome: "Jao",
+    dizerOla: function () {
+        console.log("olá me chamo " + this.nome)  
+    }
+}
+
+pessoa.dizerOla()
